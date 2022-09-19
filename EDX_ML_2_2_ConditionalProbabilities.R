3b93550d3d0e84b6ec6aad13f58093fdc2ee9852
Nipun <shehanatuk@gmail.com>
2022-09-19 14:55:43 -0400

    added more to EDX_ML_2_2_ConditionalProbabilities.R

--- EDX_ML_2_2_ConditionalProbabilities.R
+++ EDX_ML_2_2_ConditionalProbabilities.R
@@ -42,7 +42,17 @@ dat <- MASS::mvrnorm(n = 10000, c(69, 69), Sigma) %>%
 
 ps <- seq(0, 1, 0.1)
 dat %>%
+  
   mutate(g = cut(x, quantile(x, ps), include.lowest=TRUE)) %>%
   group_by(g) %>%
-  summarize(y = (y), x = (x)) %>%
+  summarize(y = mean(y), x = mean(x)) %>%
+  
+  # mutate(g = cut(x, quantile(x, ps), include.lowest = TRUE)) %>%
+  # group_by(g) %>%
+  # summarize(y = mean(y), x = mean(x)) %>%
+  
+  # mutate(g = cut(x, quantile(x, ps))) %>%
+  # group_by(g) %>%
+  # summarise(y = mean(y), x = mean(x)) %>%
+
   qplot(x, y, data=.)
---
+++ EDX_ML_3_1_Linear_Regression.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(HistData)
+
+galton_heights <- GaltonFamilies %>%
+  filter(childNum == 1 & gender == "male") %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight)
