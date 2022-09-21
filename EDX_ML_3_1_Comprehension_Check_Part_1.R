83802260c1f665bf2ad354314e66af3a6f66acaf
Nipun <shehanatuk@gmail.com>
2022-09-21 11:41:26 -0400

    added more Section 3 files

--- EDX_ML_3_1_Comprehension_Check_Part_1.R
+++ EDX_ML_3_1_Comprehension_Check_Part_1.R
@@ -10,6 +10,8 @@
 # Report all answers to at least 3 significant digits.
 library(tidyverse)
 library(caret)
+library(magrittr)
+
 
 # set.seed(1) # if using R 3.5 or earlier 
 set.seed(1, sample.kind="Rounding") # if using R 3.6 or later 
@@ -44,8 +46,7 @@ sd(rmse)
 # Note: You only need to set the seed once before running your function; do not set a seed within your function. Also be sure to use sapply() or map() as you will get different answers running the simulations individually due to setting the seed.
 
 # sapply() function takes a list, vector, or data frame as input and gives output in vector or matrix format. It is useful for operations on list objects and returns a list object of same length of original set. Sapply function in R does the same job as lapply() function but returns a vector
-library(tidyverse)
-library(caret)
+
 
 # set.seed(1) # if using R 3.5 or earlier 
 set.seed(1, sample.kind="Rounding") # if using R 3.6 or later 
@@ -101,7 +102,64 @@ rmse <- replicate(100, {
   sqrt(mean((y_hat - test_set$y)^2))
 })
 
+rmse
+
 mean(rmse)
 
 sd(rmse)
 
+# Q6
+
+
+# set.seed(1) # if using R 3.5 or earlier
+# set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+# Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.25, 0.75, 0.25, 1.0), 3, 3)
+Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.95, 0.75, 0.95, 1.0), 3, 3)
+dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+  data.frame() %>% setNames(c("y", "x_1", "x_2"))
+
+rmse_x_1 <- replicate(1, {
+  test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+  
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  
+  fit <- lm(y ~ x_1, data=train_set)
+  
+  y_hat <- predict(fit, newdata=test_set)
+  sqrt(mean((y_hat - test_set$y)^2))
+})
+
+print("rmse_x_1: ")
+rmse_x_1
+
+
+rmse_x_2 <- replicate(1, {
+  test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+  
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  
+  fit <- lm(y ~ x_2, data=train_set)
+  
+  y_hat <- predict(fit, newdata=test_set)
+  sqrt(mean((y_hat - test_set$y)^2))
+})
+
+print("rmse_x_2: ")
+rmse_x_2
+
+rmse_x_1_x_2 <- replicate(1, {
+  test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+  
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  
+  fit <- lm(y ~ x_1 + x_2, data=train_set)
+  
+  y_hat <- predict(fit, newdata=test_set)
+  sqrt(mean((y_hat - test_set$y)^2))
+})
+
+print("rmse_x_1_x_2: ")
+rmse_x_1_x_2
