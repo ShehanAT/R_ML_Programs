b5d8e5fe4f654a92b8e558a42d4ba4db51470676
Nipun <shehanatuk@gmail.com>
2022-11-09 20:31:29 -0500

    added more to EDX_ML_6_3_Dimension_Reduction.R

--- EDX_ML_6_3_Dimension_Reduction.R
+++ EDX_ML_6_3_Dimension_Reduction.R
@@ -87,4 +87,20 @@ summary(pc)
 summary(pc)$imp
 df_pc <- data.frame(summary(pc)$imp) 
 
+imp_df <- data.frame(summary(pc)$imp)
+imp_df <- imp_df[2,] %>%
+  gather(key = pc, value = imp)
+imp_df <- imp_df %>%
+  mutate(pc_index = as.integer(str_remove(imp_df$pc, "PC")))
+imp_df$pc <- factor(imp_df$pc,
+                    levels = imp_df$pc[order(imp_df$pc_index)])
+imp_df <- imp_df %>% mutate(cum_sum = cumsum(imp))
+
+# Use a ggplot() with the PCs on the x and the cumalative sum on the y 
+imp_df %>% filter(pc_index < 20) %>% arrange(pc_index, cum_sum) %>%
+  ggplot(aes(x = pc, y = cum_sum, fill = pc)) +
+  geom_col() + scale_y_continuous(breaks = seq(0,1,0.1)) + theme_grey()
+
+# Or use the following code to plot the percent variance explained by PC number.
+plot(summary(pc)$importance[3,])
 
