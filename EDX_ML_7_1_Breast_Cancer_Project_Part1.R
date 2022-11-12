3b24b09791b2b109b76feaa7f198f843001a4e7f
Nipun <shehanatuk@gmail.com>
2022-11-12 12:11:40 -0500

    added more to EDX_ML_7_1_Breast_Cancer_Project_Part1.R

---
+++ EDX_ML_7_1_Breast_Cancer_Project_Part1.R
@@ -0,0 +1,21 @@
+options(digits = 3)
+library(matrixStats)
+library(tidyverse)
+library(caret)
+library(dslabs)
+library(dplyr)
+
+data(brca)
+
+# In order to count the number of samples in the dataset use the nrow() function like so:
+nrow(brca$x)
+
+# In order to count the number of predictors in a matrix use the ncol() function like so:
+ncol(brca$x)
+
+# In order to calculate the proportion of the sample that are of a specifc condition 'M', use the following code:
+mean(brca$y == "M")
+
+# In order to calculate the column number with the highest mean and return its index in the dataset, use the following code:
+colMean <- apply(brca$x, 2, mean)
+which.max(colMean)
