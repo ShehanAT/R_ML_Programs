c7610860431a1b768e97eb072ed87636dc1dfd3f
Nipun <shehanatuk@gmail.com>
2022-11-19 12:00:36 -0500

    added more to EDX_ML_7_1_Breast_Cancer_Project_Part3.R

--- EDX_ML_7_1_Breast_Cancer_Project_Part3.R
+++ EDX_ML_7_1_Breast_Cancer_Project_Part3.R
@@ -20,3 +20,24 @@ mean(train_y == "B")
 
 mean(test_y == "B")
 
+# Q10
+
+predict_kmeans <- function(x, k) {
+  centers <- k$centers # extract cluster centers 
+  # calculate distance to cluster centers 
+  distances <- sapply(1:nrow(x), function(i){
+    apply(centers, 1, function(y) dist(rbind(x[i,], y)))
+  })
+  max.col(-t(distances)) # select cluster with min distance to center 
+}
+
+set.seed(3) # This sets a seed for R's random number generator
+
+#The kMeans Clustering algorithm is an unsupervised non-linear algorithm that cluster data based on similarity or similar groups.
+# The function kmeans() return 
+k <- kmeans(train_x, centers = 2 )
+
+# The function predict_kmeans(x, k) take a matrix of observations(x) and a kMeans object(k) and assigns each row of x to a cluster from k
+predict_result <- predict_kmeans(train_x, k)
+predict_result
+
